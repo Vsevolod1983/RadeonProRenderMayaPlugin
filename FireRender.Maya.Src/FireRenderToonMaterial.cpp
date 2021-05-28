f5911d61e24c06f0ba6fd2f3a058d3b510f296e8
Vsevolod Golovko <vgolovko@luxoft.com>
2021-05-28 11:48:56 +0300

    we want setup color input always (in advanced mode and in simple mode)

--- FireRender.Maya.Src/FireRenderToonMaterial.cpp
+++ FireRender.Maya.Src/FireRenderToonMaterial.cpp
@@ -148,6 +148,8 @@ frw::Shader FireMaya::ToonMaterial::GetShader(Scope& scope)
 	bool advancedMode = shaderNode.findPlug(Attribute::showAdvanced, false).asBool();
 	bool mixLevels = shaderNode.findPlug(Attribute::showMixLevels, false).asBool();
 
+	shader.SetValue(RPR_MATERIAL_INPUT_COLOR, scope.GetValue(shaderNode.findPlug(Attribute::color, false)));
+
 	if (advancedMode)
 	{
 		shader.SetValue(RPR_MATERIAL_INPUT_COLOR, frw::Value(1.0, 1.0, 1.0, 1.0));
@@ -173,10 +175,6 @@ frw::Shader FireMaya::ToonMaterial::GetShader(Scope& scope)
 		}
 
 		shader.SetValue(RPR_MATERIAL_INPUT_DIFFUSE_RAMP, toonRamp);
-	}
-	else
-	{
-		shader.SetValue(RPR_MATERIAL_INPUT_COLOR, scope.GetValue(shaderNode.findPlug(Attribute::color, false)));
 	}
 
 	shader.SetValue(RPR_MATERIAL_INPUT_ROUGHNESS, scope.GetValue(shaderNode.findPlug(Attribute::roughness, false)));
