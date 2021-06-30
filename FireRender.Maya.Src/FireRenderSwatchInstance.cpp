dd62b1e7a7b21df0d849c5c0a4eb05ac3b49517e
Vsevolod Golovko <vgolovko@luxoft.com>
2021-06-30 17:42:48 +0300

    NS swatches for Windows only for now

--- FireRender.Maya.Src/FireRenderSwatchInstance.cpp
+++ FireRender.Maya.Src/FireRenderSwatchInstance.cpp
@@ -27,8 +27,10 @@ FireRenderSwatchInstance::FireRenderSwatchInstance()
 
 void FireRenderSwatchInstance::initScene()
 {
+#ifdef _WIN32
 	// force using NorthStar for swatches
 	context.SetPluginEngine(TahoePluginVersion::RPR2);
+#endif
 
 	backgroundRendererBusy = false;
 	m_warningDialogOpen = false;
